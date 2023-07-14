701357d6a4a82f841ee9f5b3714e8a63e23fb7b1
Omar-Arja <omaralarja17@gmail.com>
2023-07-14 12:31:19 +0300

    added winner and draw system

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const cells = document.querySelectorAll('.cell');
+const start_game = document.getElementById('start-game');
+const game = document.getElementById('game');
+const landing_page = document.getElementById('landing-page');
+const game_over_div = document.getElementById('game-over');
+let player_Name_1 = document.getElementById('player-1');
+let score_1 = document.getElementById('score-1');
+let player_Name_2 = document.getElementById('player-2');
+let score_2 = document.getElementById('score-2');
+let game_over_text = document.getElementById('game-over-text');
+const continue_btn = document.getElementById('continue');
+const restart_btn = document.getElementById('restart');
+let score_x = 0;
+let score_o = 0;
+let winner = null;
 const player_x = 'X';
 const player_o = 'O';
 let turn = player_x;
 let game_over = false;
 
+
 let board_state = Array(cells.length).fill(null);
-
 const strike = document.getElementById('strike');
-
-setHover();
 
 const winning_combinations = [
     { combo: [0, 1, 2], strike_class: 'strike-row-1' },
@@ -20,7 +32,14 @@ const winning_combinations = [
     { combo: [0, 4, 8], strike_class: 'strike-diagonal-1' },
     { combo: [2, 4, 6], strike_class: 'strike-diagonal-2' }];
 
+
+setHover();
 cells.forEach(cell => cell.addEventListener('click', handleClick));
+start_game.addEventListener('click', startGame);
+continue_btn.addEventListener('click', reset);
+restart_btn.addEventListener('click', () => {
+    window.location.reload();
+});
 
 function handleClick(e) {
     if (game_over) return;
@@ -53,9 +72,79 @@ function setHover() {
 function checkWinner() {
     winning_combinations.forEach(combination => {
         if (board_state[combination.combo[0]] == board_state[combination.combo[1]] &&
-            board_state[combination.combo[1]] == board_state[combination.combo[2]] && board_state[combination.combo[0]] != null) {
-                strike.classList.add(combination.strike_class);
-                game_over = true;
+            board_state[combination.combo[1]] == board_state[combination.combo[2]] &&
+            board_state[combination.combo[0]] != null) {
+            strike.classList.add(combination.strike_class);
+            if (board_state[combination.combo[0]] == player_x) {
+                score_x++;
+                winner = player_Name_1;
             }
+            else {
+                score_o++;
+                winner = player_Name_2;
+            }
+            game_over = true;
+            score_1.innerText = score_x;
+            score_2.innerText = score_o;
+            gameOver();
+            return;
+
+        }
     })
+    if (!board_state.includes(null) && !game_over) {
+        game_over = true;
+        gameOver();
+        winner = null;
+    }
+}
+
+function gameOver() {
+    if (game_over) {
+        game_over_div.classList.remove('hide');
+        if (winner != null) {
+            game_over_text.innerText = `${winner.innerText} Wins!`;
+        }
+        else {
+            game_over_text.innerText = "It's a Draw!";
+        }
+    }
+    else {
+        game_over_div.classList.add('hide');
+    }
+}
+
+function reset() {
+    cells.forEach(cell => {
+        cell.innerText = "";
+        cell.classList.remove('hover-x');
+        cell.classList.remove('hover-o');
+    })
+    board_state = Array(cells.length).fill(null);
+    turn = player_x;
+    winner = null;
+    game_over = false;
+    gameOver();
+    strike.classList.remove(
+        'strike-row-1',
+        'strike-row-2',
+        'strike-row-3',
+        'strike-column-1',
+        'strike-column-2',
+        'strike-column-3',
+        'strike-diagonal-1',
+        'strike-diagonal-2')
+    setHover();
+}
+
+function startGame() {
+    let player1 = document.getElementById('player1').value;
+    let player2 = document.getElementById('player2').value;
+    if (player1 == "") player1 = "X";
+    if (player2 == "") player2 = "O";
+    landing_page.className = 'hide';
+    game.className = 'show';
+    player_Name_1.innerText = player1;
+    player_Name_2.innerText = player2;
+    score_1.innerText = score_x;
+    score_2.innerText = score_o;
 }
